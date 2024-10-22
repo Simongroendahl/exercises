43a58a76252833bb3b4cce1de26efbf438bd4bba
Simon <sila0002@stud.kea.dk>
2024-10-22 20:55:36 +0200

    Made colour switcher dependent on list choice.

--- to_do_app/main.js
+++ to_do_app/main.js
@@ -1,113 +1,4 @@
 
-
-// const inputBox = document.getElementById("input_box");
-// const quantityBox = document.getElementById("input_quantity");
-// const listContainer = document.getElementById("list_container");
-// const completedListContainer = document.getElementById("completed_list_container");
-// const addButton = document.getElementById("addButton");
-
-// addButton.addEventListener("click", addTask);
-// inputBox.addEventListener("click", expandOptions);
-
-// function expandOptions() {
-//     quantityBox.classList.remove("hide");
-// }
-
-// function addTask() {
-//     console.log("Add Task is running");
-//     if(inputBox.value === '') {
-//         alert("No text was added. Try again")
-//     } else {
-//         let rowContainer = document.createElement("div");
-//         let li = document.createElement("li");
-//         li.innerHTML = inputBox.value;
-
-//         let quantity = quantityBox.value;
-//         if (quantity) {
-//             li.innerHTML += ` x${quantity} `;
-//         }
-
-//         let span = document.createElement("span");
-//         span.innerHTML = "\u00d7";
-//         li.appendChild(span);
-
-//         let rowDivider = document.createElement("div");
-//         rowDivider.classList.add("row_divider_line");
-
-
-//         rowContainer.appendChild(li);
-//         rowContainer.appendChild(rowDivider);
-//         rowContainer.setAttribute("checked-filter", "unchecked");
-
-
-
-//         listContainer.appendChild(rowContainer);
-//     }
-//     inputBox.value = "";
-//     quantityBox.value = "";
-
-//     quantityBox.classList.add("hide");
-//     gemData();
-// }
-
-// listContainer.addEventListener("click", function(listcheck){
-//     console.log("Checkboxes will be checked off");
-//     if(listcheck.target.tagName === "LI") {
-//         listcheck.target.classList.toggle("checked");
-
-//         if (listcheck.target.classList.contains("checked")) {
-//             listcheck.target.setAttribute("checked-filter", "checked");
-//         } else {
-//             listcheck.target.removeAttribute("checked-filter");
-//         }
-
-//         gemData();
-//         //sortList()
-//     } else if(listcheck.target.tagName === "SPAN"){
-//         listcheck.target.parentElement.parentElement.remove();
-//         gemData();
-//         // sortList()
-//     }
-// }, false);
-
-// // Forsøg 2
-// function removeOrphanRowDividers() {
-//     const children = listContainer.children;
-//     for (let i = 0; i < children.length; i++) {
-//         const child = children[i];
-//         // Check if the child is a row divider
-//         if (child.classList.contains("row_divider_line")) {
-//             // Check if its parent has a previous sibling that is an LI
-//             const parentDiv = child.parentElement;
-//             if (!parentDiv.parentElement ||
-//                 parentDiv.parentElement.tagName !== "LI") {
-//                 // Remove the row divider and its parent div
-//                 parentDiv.remove();
-//                 i--; // Decrement i to account for the removed element
-//             }
-//         }
-//     }
-// }
-
-// function gemData() {
-//     localStorage.setItem("data", listContainer.innerHTML);
-// }
-
-// function visData() {
-//     listContainer.innerHTML = localStorage.getItem("data");
-//     //localStorage.clear();
-// }
-
-// visData();
-
-
-
-// removeOrphanRowDividers();
-
-
-// FORSØG 2
-
-
 const inputBox = document.getElementById("input_box");
 const quantityBox = document.getElementById("input_quantity");
 const listContainer = document.getElementById("list_container");
@@ -129,6 +20,7 @@ function addTask() {
         let rowContainer = document.createElement("div");
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
+        li.setAttribute("contenteditable", "true");
 
         let quantity = quantityBox.value;
         if (quantity) {
@@ -147,34 +39,19 @@ function addTask() {
         rowContainer.setAttribute("data-checked", "false");
 
         listContainer.appendChild(rowContainer);
+        gemData();
     }
     inputBox.value = "";
     quantityBox.value = "";
     quantityBox.classList.add("hide");
-    gemData();
+
 }
 
-// listContainer.addEventListener("click", function (event) {
-//     console.log("Checkboxes will be checked off");
-//     if (event.target.tagName === "LI") {
-//         event.target.classList.toggle("checked");
-
-//         const rowContainer = event.target.parentElement; // Get the rowContainer
-
-//         if (event.target.classList.contains("checked")) {
-//             rowContainer.setAttribute("data-checked", "true");
-//             completedListContainer.appendChild(rowContainer); // Move to completedListContainer
-//         } else {
-//             rowContainer.setAttribute("data-checked", "false");
-//             listContainer.appendChild(rowContainer); // Move back to listContainer
-//         }
-
-//         gemData();
-//     } else if (event.target.tagName === "SPAN") {
-//         event.target.parentElement.parentElement.remove();
-//         gemData();
-//     }
-// }, false);
+listContainer.addEventListener("blur", function(event) {
+    if (event.target.tagName === "LI") {
+        gemData(); // Save data when the user finishes editing
+    }
+}, true);
 
 listContainer.addEventListener("click", function (event) {
     console.log("Checkboxes will be checked off");
@@ -234,15 +111,65 @@ function removeOrphanRowDividers() {
 }
 
 function gemData() {
-    localStorage.setItem("data", listContainer.innerHTML);
+    const listData = listContainer.innerHTML;
+    const completedData = completedListContainer.innerHTML;
+
+    localStorage.setItem("listData", listData);
+    localStorage.setItem("completedData", completedData);
 }
 
 function visData() {
-    listContainer.innerHTML = localStorage.getItem("data");
+    const storedListData = localStorage.getItem("listData");
+    const storedCompletedData = localStorage.getItem("completedData");
+
+    if (storedListData) {
+        listContainer.innerHTML = storedListData;
+    }
+    if (storedCompletedData) {
+        completedListContainer.innerHTML = storedCompletedData;
+    }
 }
 
 visData();
 removeOrphanRowDividers();
+
+// FARVE UPDATE
+
+
+const asideMenu = document.getElementById("asideBar");
+const contentBg = document.getElementById("contentContainer");
+const groceryButton = document.getElementById("groceryButton");
+const toDoButton = document.getElementById("toDoButton");
+
+window.addEventListener("load", defaultMode);
+
+// Denne funktion startes når dropdown menuens værdi ændres og kigger derefter options value.
+// Her kigger farveUpdater-funktionen efter værdien, som ændre bodyColor variablens attributes.
+
+function farveUpdater(farve) {
+    if (farve === "yellow") {
+        asideMenu.setAttribute("data-theme", "yellow-theme");
+        contentBg.setAttribute("data-theme", "yellow-theme")
+    } else if (farve === "purple") {
+        asideMenu.setAttribute("data-theme", "purple-theme");
+        contentBg.setAttribute("data-theme", "purple-theme");
+};}
+
+toDoButton.addEventListener("click", (nyFarve) => {
+    farveUpdater(nyFarve.target.value);
+});
+
+groceryButton.addEventListener("click", (nyFarve) => {
+    farveUpdater(nyFarve.target.value);
+});
+
+
+
+function defaultMode() {
+    asideMenu.setAttribute("data-theme", "yellow-theme");
+    contentBg.setAttribute("data-theme", "yellow-theme");
+};
+
 
 
 
